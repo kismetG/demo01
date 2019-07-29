5f990b7baa5274529680ba9fbd91dd5edceaeca6
ganqizhi <2497870211@qq.com>
2019-07-29 10:13:20 +0800

    测试1

---
+++ src/com/czxy/demo/test01.java
@@ -0,0 +1,12 @@
+package com.czxy.demo;
+
+/**
+ * @Author dell
+ * @Version v 1.0
+ * @Date 2019/7/29
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("yyyyyyyyyyyyyyyyyy");
+    }
+}
